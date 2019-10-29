1c945bc05c49c00bef1b26ed2ffc22438611b76f
george eleftheriou <canobi2002@yahoo.com>
2019-10-29 12:37:27 +0200

    working ok

--- rename_files.py
+++ rename_files.py
@@ -1,7 +1,4 @@
-from tkinter import *
-from tkinter import filedialog
-from tkinter import scrolledtext
-import collections
+from tkinter import Button, Label, Entry, Tk, END, filedialog, scrolledtext
 from pathlib import Path
 import re
 
@@ -20,21 +17,22 @@ directory1 = ''
 listPreview = []
 listFiles = []
 txtArea1 = scrolledtext.ScrolledText(window, width=50, height=80)
-txtArea1.grid(column=0, row=4)
+txtArea1.grid(column=0, row=5)
 txtArea2 = scrolledtext.ScrolledText(window, width=50, height=80)
-txtArea2.grid(column=1, row=4)
+txtArea2.grid(column=1, row=5)
 txtArea3 = scrolledtext.ScrolledText(window, width=50, height=80)
-txtArea3.grid(column=2, row=4)
+txtArea3.grid(column=2, row=5)
 
 
 def openFileDialog():
     global directory1
     directory1 = filedialog.askdirectory()  # returns string
     txtArea1.delete("1.0", END)
+    txtArea2.delete("1.0", END)
+    txtArea3.delete("1.0", END)
     lbl.configure(text=directory1)
-    listFiles = [p.name for p in Path(directory1).iterdir()]
-    for file in listFiles:
-        txtArea1.insert('end', file)
+    for file in Path(directory1).iterdir():
+        txtArea1.insert('end', file.name)
         txtArea1.insert('end', '\n')
 
 
@@ -44,33 +42,37 @@ def renamePreview():
     pattern = txt1.get()
     pattern2 = txt2.get()
     result = ''
-    listPreview = []
-    for file in [p.name for p in Path(directory1).iterdir()]:
-        if re.search(pattern, file, flags=re.I):
+    for file in Path(directory1).iterdir():
+        if re.search(pattern, file.name, flags=re.I):
             result = re.sub(pattern, pattern2,
-                            file, flags=re.I)
-            listPreview.append(result)
-    for file in listPreview:
-        txtArea2.insert('end', file)
-        txtArea2.insert('end', '\n')
+                            file.name, flags=re.I)
+            txtArea2.insert('end', result)
+            txtArea2.insert('end', '\n')
 
 
 def rename():
     global directory1
     txtArea3.delete("1.0", END)
-    pattern = txt1.get()
-    pattern2 = txt2.get()
-    listOutput = []
+    pattern = re.escape(txt1.get())
+    pattern2 = re.escape(txt2.get())
     result = ''
-    for file in [p for p in Path(directory1).iterdir()]:
+    for file in Path(directory1).iterdir():
         if re.search(pattern, file.name, flags=re.I):
             result = re.sub(pattern, pattern2,
                             file.name, flags=re.I)
-            listOutput.append(result)
-            # file.rename(result)
-    for file in listOutput:
-        txtArea3.insert('end', file)
-        txtArea3.insert('end', '\n')
+            file.rename(file.parent.joinpath(result))
+            txtArea3.insert('end', result)
+            txtArea3.insert('end', '\n')
+
+
+def refresh():
+    global directory1
+    txtArea1.delete("1.0", END)
+    txtArea2.delete("1.0", END)
+    txtArea3.delete("1.0", END)
+    for file in Path(directory1).iterdir():
+        txtArea1.insert('end', file.name)
+        txtArea1.insert('end', '\n')
 
 
 btnFileDialog = Button(
@@ -79,7 +81,10 @@ btnRenamePreview = Button(
     window, text="Rename Files Preview", command=renamePreview)
 btnRename = Button(
     window, text="Rename Files", command=rename)
-btnFileDialog.grid(column=0, row=3)
-btnRenamePreview.grid(column=1, row=3)
-btnRename.grid(column=2, row=3)
+btnRefresh = Button(
+    window, text="Refresh Directory", command=refresh)
+btnRefresh.grid(column=0, row=3)
+btnFileDialog.grid(column=0, row=4)
+btnRenamePreview.grid(column=1, row=4)
+btnRename.grid(column=2, row=4)
 window.mainloop()
